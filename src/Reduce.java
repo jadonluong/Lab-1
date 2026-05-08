65edbb52a8948c8d76ab8f9da2841532ea95cb78
Jadon Luong <jadonluong@MacBook-Pro-318.local>
2026-05-08 13:52:05 -0400

    attempted task 5

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int c = 0;
+
+        while (i > 0) {
+
+            boolean divisibleBy2 = i % 2 == 0;
+
+            if (divisibleBy2) {
+                i = i/2;
+            }
+            else {
+                i = i - 1;
+            }
+            c++;
+        }
+        System.out.println(c);
+    }
 }
